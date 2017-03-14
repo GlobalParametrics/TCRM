28fa89a2ba639661ae82e3c43a906715dd0f6183
Claire Krause <claire.krause@ga.gov.au>
2017-03-14 11:21:26 +1100

    added beta and cleaned input/output file formats

--- Evaluate/interpolateTracks.py
+++ Evaluate/interpolateTracks.py
@@ -8,32 +8,16 @@ from scipy.interpolate import interp1d, splev, splrep
 
 from Utilities.maputils import latLon2Azi
 from Utilities.loadData import loadTrackFile
-from Utilities.track import Track, ncSaveTracks
+from Utilities.track import Track, ncSaveTracks, TCRM_COLS, TCRM_FMTS
 
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.NullHandler())
 
-TRACKFILE_COLS = ('Indicator', 'CycloneNumber', 'Year', 'Month',
-                  'Day', 'Hour', 'Minute', 'TimeElapsed', 'Datetime',
-                  'Longitude', 'Latitude', 'Speed', 'Bearing',
-                  'CentralPressure', 'WindSpeed', 'rMax',
-                  'EnvPressure')
-
-TRACKFILE_FMTS = ('i', 'i', 'i', 'i',
-                  'i', 'i', 'i', 'f', datetime,
-                  'f', 'f', 'f', 'f', 'f',
-                  'f', 'f', 'f', 'f')
-
-TRACKFILE_OUTFMT = ('%i,%i,%i,%i,'
-                    '%i,%i,%i,%8.3f,%s,%5.1f,'
-                    '%8.3f,%8.3f,%6.2f,%6.2f,%7.2f,'
-                    '%6.2f,%6.2f,%7.2f')
-
 OUTPUT_COLS = ('CycloneNumber', 'Datetime', 'TimeElapsed', 'Longitude',
                   'Latitude', 'Speed', 'Bearing', 'CentralPressure',
-                  'EnvPressure', 'rMax')
+                  'EnvPressure', 'rMax', 'beta')
 
-OUTPUT_FMTS = '%i,%s,%7.3f,%8.3f,%8.3f,%6.2f,%6.2f,%7.2f,%7.2f,%6.2f'
+OUTPUT_FMTS = '%i,%s,%7.3f,%8.3f,%8.3f,%6.2f,%6.2f,%7.2f,%7.2f,%6.2f,%6.3f'
 
 class Track2(object):
     def __init__(self, data):
@@ -117,7 +101,8 @@ def interpolate(track, delta, interpolation_type=None):
 
         npEnv = interp1d(timestep, track.EnvPressure, kind='linear')(newtime)
         nrMax = interp1d(timestep, track.rMax, kind='linear')(newtime)
-
+        nbeta = interp1d(timestep, track.beta, kind='linear')(newtime)
+        
     else:
         if interpolation_type == 'akima':
             # Use the Akima interpolation method:
@@ -181,6 +166,7 @@ def interpolate(track, delta, interpolation_type=None):
 
         npEnv = interp1d(timestep, track.EnvPressure, kind='linear')(newtime)
         nrMax = interp1d(timestep, track.rMax, kind='linear')(newtime)
+        nbeta = interp1d(timestep, track.beta, kind='linear')(newtime)
 
     if len(nLat) >= 2:
         bear_, dist_ = latLon2Azi(nLat, nLon, 1, azimuth=0)
@@ -210,14 +196,14 @@ def interpolate(track, delta, interpolation_type=None):
 
     newdata = np.empty(len(newtime),
                         dtype={
-                               'names': TRACKFILE_COLS,
-                               'formats': TRACKFILE_FMTS
+                               'names': TCRM_COLS,
+                               'formats': TCRM_FMTS
                                } )
 
-    for key, val in zip(TRACKFILE_COLS,
-                        [newindex, newTCID, nYear, nMonth, nDay, nHour, nMin,
-                           newtime, newdates, nLon, nLat, nvFm, nthetaFm,
-                           npCentre, nwSpd, nrMax, npEnv]):
+    for key, val in zip(TCRM_COLS,
+                        [newTCID, newdates, newtime,
+                           nLon, nLat, nvFm, nthetaFm,
+                           npCentre, npEnv, nrMax, nbeta]):
         newdata[key] = val
     newtrack = Track(newdata)
     newtrack.trackId = track.trackId
